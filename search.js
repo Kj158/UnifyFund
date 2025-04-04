6ab01e3ad7579abe6998455d6aaaec0c8756fc31
Kashish Jain <153837382+Kj158@users.noreply.github.com>
2025-04-04 16:24:38 +0530

    Update search.js

--- search.js
+++ search.js
@@ -14,22 +14,5 @@ function search() {
     }
   
 }
-function expandCard(card)
-{card.cardList.toggle("expanded");}
 
-function readmore() {
 
-  var button = document.getElementById('button');
-  var more = document.getElementsByClassName('more');
-  more.style.display= (content.style.display==='none'|| content.style.display==='')? 'block':'none';
-  if (dots.style.display === "none"){
-    dots.style.display="Inline";
-    more.style.display="none";
-    button.innerHTML="Read more";
-  }else{
-    dots.style.display="none";
-    more.style.display="inline";
-    button.innerHTML="Read less";
-  }
-
-}
